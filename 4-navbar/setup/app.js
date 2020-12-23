bf581c713b2def81e31a2e4d9613eb7142e39d6c
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-23 13:48:04 -0600

    add nav

--- 4-navbar/setup/app.js
+++ 4-navbar/setup/app.js
@@ -1,5 +1,14 @@
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
+// Elements
+const toggleElement = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.links');
+
+// State
+let show = false;
+
+// Event Listeners
+toggleElement.addEventListener('click', toggle);
+
+// Functions
+function toggle() {
+  navLinks.classList.toggle('show-links');
+}
